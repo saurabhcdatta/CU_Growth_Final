16264c5164af89008686f77ddacf46298cbd400f
Saurabh Datta <saurabh.charles.datta@gmail.com>
2026-09-05 06:19:31 -0400

    Add files via upload

--- test.R
+++ test.R
@@ -1,9 +1,17 @@
-hist %>% filter(q_index %in% c(N_Q - 20, N_Q)) %>%
-  mutate(cat = cut(assets_tot, BREAKS, CAT_LABELS, right = FALSE)) %>%
-  select(join_number, q_index, cat) %>%
-  pivot_wider(names_from = q_index, values_from = cat) %>%
-  setNames(c("jn", "then", "now")) %>%
-  filter(!is.na(then), !is.na(now)) %>%
-  summarise(up = sum(as.integer(now) > as.integer(then)),
-            down = sum(as.integer(now) < as.integer(then)),
-            same = sum(now == then))
+for (H in c(4, 12, 20)) {
+  us <- feat[[paste0("usable_surv_h", H)]]
+  d <- feat[us, ]; d$dy <- d[[paste0("dy_h", H)]]
+  d <- d[!is.na(d$dy) & d$cat_k == 6, ]
+  cat("\nh =", H, "\n")
+  print(d %>% mutate(yr = START_YEAR + (q_index - 1) %/% 4) %>%
+          group_by(yr) %>%
+          summarise(n = n(),
+                    med_ann = round(100 * (exp(median(dy) * 4 / H) - 1), 2),
+                    .groups = "drop") %>% as.data.frame())
+}
+
+delta_q <- (log(1 + recent_ann/100) - log(1 + hist_ann/100)) / 4   # per quarter
+POOLS[["20"]][["6"]]$dy <- POOLS[["20"]][["6"]]$dy + delta_q * 20
+POOLS[["20"]][["7"]]$dy <- POOLS[["20"]][["7"]]$dy + delta_q * 20
+PROB[["20"]] <- emp_bucket_probs(POOLS[["20"]], fc$cat_k, fc$y_raw)
+round(colSums(PROB[["20"]]), 1)
